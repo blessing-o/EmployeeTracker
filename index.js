7f1167238d1694d32c12ca9bf41d51ba3a840aa3
blessing-o <oghaleokeme@gmail.com>
2024-01-09 05:48:04 -0500

    added the options for the users to choose and I connected the database

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const fs = require ('fs');
+// const fs = require ('fs');
+const mysql = require('mysql2');
 const inquirer = require ('inquirer');
+
+function action(s){
+
+}
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'employer_db'
+    },
+    console.log(`Connected to the employer_db database.`)
+  );
 
 function init(){
     inquirer
         .prompt([
             {type: "list",
-            message: "",
-            name: "",
-            choices: ["view all departments", "view all roles", ""]
-
+            message: "Select one of the following options",
+            name: "choice",
+            choices: ["view all departments", "view all roles","view all employees","add a department", "add a role", "add an employee", "update an employee role"]
             },
         ])
+        .then((choice)=>{
+            action(choice); 
+        })
 }
