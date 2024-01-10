e245147e1a638574bef3cf8aef056863de811252
blessing-o <oghaleokeme@gmail.com>
2024-01-10 07:23:36 -0500

    added the view all departments functionality

--- index.js
+++ index.js
@@ -2,9 +2,63 @@
 const mysql = require('mysql2');
 const inquirer = require ('inquirer');
 
-function action(s){
-
+function viewAllDepartments(){
+    const sql = 'SELECT * FROM department';
+    db.query(sql,(err, rows)=>{
+        if(err) throw err;
+        console.log(rows);
+    })
 }
+
+// function viewAllRoles(){
+//     const sql = 'SELECT * FROM role';
+//     db.query(sql,(err, rows)=>{
+//         if(err) throw err;
+//         console.log(rows);
+//     })
+// }
+
+// function addAdepartment(){
+//     inquirer
+//         .prompt([
+//             {input: "list",
+//             message: "what's the name of the department you want to add?",
+//             name: "department",
+//             },
+//         ])
+//         .then((dep)=>{
+//             action(choice); 
+//         })
+
+// }
+
+function action(choice) {
+    switch (choice.choice) {
+      case 'view all departments':
+        viewAllDepartments();
+        break;
+      case 'view all roles':
+        viewAllRoles()
+        break;
+      case 'view all employees':
+        // Implement logic to view all employees
+        break;
+      case 'add a department':
+        // Implement logic to add a department
+        break;
+      case 'add a role':
+        // Implement logic to add a role
+        break;
+      case 'add an employee':
+        // Implement logic to add an employee
+        break;
+      case 'update an employee role':
+        // Implement logic to update an employee role
+        break;
+      default:
+        console.log('Invalid choice. Please choose a valid option.');
+    }
+  }
 
 // Connect to database
 const db = mysql.createConnection(
@@ -32,3 +86,5 @@ function init(){
             action(choice); 
         })
 }
+
+init();
